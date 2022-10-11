313b74b9573b03e8deff5271039f1758f56bb8a4
JuHee Kim <rlawngml59@gmail.com>
2022-10-11 13:02:18 +0900

    dogapp.app
    - migrate

--- teamproject/teamproject/urls.py
+++ teamproject/teamproject/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dogapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('appdog/', include('appdog.urls')),
+    path('dogapp/', include('dogapp.urls')),
 ]
