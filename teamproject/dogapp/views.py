6bae82a2b5e618e4f7e90a9cadc5383dd336e450
JuHee Kim <rlawngml59@gmail.com>
2022-10-11 14:47:27 +0900

    dogapp.app
    -게시판 만들기

--- teamproject/dogapp/views.py
+++ teamproject/dogapp/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
+from django.utils import timezone
 
 def index(request):
-    return HttpResponse("setting")
+    """ 목록 출력"""
+    question_list = Question.objects.order_by('-create_date')
+    context={'question_list':question_list}
+    return render(request, 'dogapp/question_list.html', context)
 
+def detail(request, question_id):
+    """질문 내용 출력"""
+    question=get_object_or_404(Question, pk=question_id)
+    context={'question':question}
+    return render(request, 'dogapp/question_detail.html', context)
+
+def answer_create(request, question_id):
+    """답변 등록"""
+    question=get_object_or_404(Question,pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('dogapp:detail', question_id=question.id)
